8d54f16078c9da08600ef89ee7a060c42fbb8ec9
Gollapally Karunakar <gollapally@blackswan-technologies.com>
2019-05-07 10:24:30 +0530

    integrated Api for popover sliders

--- src/app/modules/source-management/template-renderer/template-renderer.component.ts
+++ src/app/modules/source-management/template-renderer/template-renderer.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ICellRendererAngularComp } from 'ag-grid-angular';
 import { FormsModule } from '@angular/forms';
+import { SourceManagementService } from '../source-management.service';
 
 @Component({
   selector: 'app-template-renderer',
@@ -8,6 +9,8 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./template-renderer.component.scss']
 })
 export class TemplateRendererComponent implements OnInit {
+  changedSliderValue:any = [];
+  mainChangedSliderValue:any = [];
   public sliderValue:any;
   public popoverData:any = [];
   public popoverHeaderData:any = {};
@@ -25,39 +28,121 @@ export class TemplateRendererComponent implements OnInit {
     2: "MEDIUM",
     3: "HIGH"
   };
+
+  constructor(private _sourceManagementService:SourceManagementService) { }
   
   agInit(params:any,event): void {
     this.popoverHeaderData = {},
     this.popoverData = [];
     this.dataAttributesForPopover = [];
+    this.changedSliderValue = [];
+    this.mainChangedSliderValue = [];
     this.popoverData.push(params);
     //console.log('params: ', params);
     this.popoverHeaderData = {
       company_name : this.popoverData[0].data.source ? this.popoverData[0].data.source : '',
       field_name: this.popoverData[0].colDef.headerName ? this.popoverData[0].colDef.headerName : ''
     };
-    if(this.popoverData[0].value && this.popoverData[0].value.dataAttributes !== undefined && this.popoverData[0].value.dataAttributes.length > 0){
-      this.dataAttributesForPopover = this.popoverData[0].value.dataAttributes;
-    }
-    if(this.popoverData[0].value){
-      this.sliderValue = this.popoverData[0].value.subClassificationCredibility ? this.popoverData[0].value.subClassificationCredibility : '';
+    if(this.popoverData[0] && this.popoverData[0].value){
+      var colIndex = this.popoverData[0].value.classifications[0].subClassifications.findIndex(x => x.subClassifcationName === this.popoverData[0].colDef.headerName);
+      if(this.popoverData[0].value.classifications[0] && this.popoverData[0].value.classifications[0].subClassifications){
+        this.sliderValue = this.popoverData[0].value.classifications[0].subClassifications[colIndex].subClassificationCredibility ? this.popoverData[0].value.classifications[0].subClassifications[colIndex].subClassificationCredibility : '';
+        this.mainChangedSliderValue.push(this.credibility_digit_map[this.sliderValue]);
+
+        if(this.popoverData[0].value.classifications[0].subClassifications[colIndex].dataAttributes !== undefined && this.popoverData[0].value.classifications[0].subClassifications[colIndex].dataAttributes.length > 0){
+          this.dataAttributesForPopover = this.popoverData[0].value.classifications[0].subClassifications[colIndex].dataAttributes;
+
+          for(let j=0;j<this.dataAttributesForPopover.length;j++){
+            this.changedSliderValue.push(this.credibility_digit_map[this.dataAttributesForPopover[j].credibilityValue]);
+          };
+        }
+      }
     }
   }
-  constructor() { }
+  
   ngOnInit() {
   }
 
+  mainSliderValueChange(e){
+    this.mainChangedSliderValue = [];
+    this.changedSliderValue = [];
+    const currentValMain = (e.target && e.target.value) ? e.target.value : 0;
+    let mainSliderValue = this.credibility_text_map[currentValMain];
+    for(let i=0;i<this.dataAttributesForPopover.length;i++){
+      this.dataAttributesForPopover[i].credibilityValue = mainSliderValue;
+      this.mainChangedSliderValue.push(this.credibility_digit_map[mainSliderValue]);
+    };
+    this.changedSliderValue = this.mainChangedSliderValue;
+  }
   /**Change value onChange of slider from popover */
   sliderValueChangeInPopover(e){
     const currentVal = (e.target && e.target.value) ? e.target.value : 0;
     e.target.previousElementSibling.textContent = this.credibility_text_map[currentVal];
-    this.temp.push({
-      value: this.credibility_text_map[currentVal]
-    })
-    console.log("On change=======>",this.temp);
   }
+
+  /**saving the source after updating Credibility in popover*/
+  public saveSourceUpdatedData = {};
+  saveSource(e){
+      this.saveSourceUpdatedData = {};
+      this.mainChangedSliderValue = [];
+      
+      for(let i=0;i<this.changedSliderValue.length;i++){
+        var currentSliderValue = this.credibility_text_map[this.changedSliderValue[i]];
+        if(currentSliderValue && currentSliderValue !== undefined){
+          var sliderIndex = i;
+          if(this.popoverData[0] && this.popoverData[0].value && this.popoverData[0].value.classifications[0] && this.popoverData[0].value.classifications[0].subClassifications){
+            var colIndex = this.popoverData[0].value.classifications[0].subClassifications.findIndex(ele => ele.subClassifcationName === this.popoverData[0].colDef.headerName);
+            
+            if(this.popoverData[0].value.classifications[0].subClassifications[colIndex].dataAttributes[sliderIndex]){
+              this.popoverData[0].value.classifications[0].subClassifications[colIndex].dataAttributes[sliderIndex].credibilityValue =  currentSliderValue;
+            }
+            if(this.popoverData[0].value.classifications[0].subClassifications[colIndex].dataAttributes){
+            var currentDataAttributes = this.popoverData[0].value.classifications[0].subClassifications[colIndex].dataAttributes;
+              for(let j=0;j<currentDataAttributes.length;j++){
+                if(currentDataAttributes[j].credibilityValue === currentSliderValue){
+                  this.sliderValue = currentSliderValue
+                }a
+                else{
+                  this.sliderValue = "NONE";
+                  this.mainChangedSliderValue.push(0);
+                  break;
+                }    
+              }
+            }
+          }
+        }
+      }
+      this.saveSourceUpdatedData = {
+        "classifications" : this.popoverData[0].value.classifications,
+        "deleteMedia" : this.popoverData[0].value.deleteMedia,
+        "entityId" : this.popoverData[0].value.entityId,
+        "sourceCreatedDate" : this.popoverData[0].value.sourceCreatedDate,
+        "sourceDisplayName" : this.popoverData[0].value.sourceDisplayName,
+        "sourceDomain" : [],
+        "sourceId" : this.popoverData[0].value.sourceId,
+        "sourceIndustry" : [],
+        "sourceJurisdiction" : [],
+        "sourceMedia" : [],
+        "sourceName" : this.popoverData[0].value.sourceName,
+        "sourceType" : this.popoverData[0].value.sourceType,
+        "sourceUrl" : this.popoverData[0].value.sourceUrl
+      };
+
+      var params = this.saveSourceUpdatedData;
+      this._sourceManagementService.updateScource(params).subscribe(data => {
+        console.log("Success........!");
+      },
+      (error => {
+        console.log(error);
+      }));
+      console.log('this.saveSourceUpdatedData: ',this.saveSourceUpdatedData);
+    }
 
   // getSlider(key){
   //   return '<span>'+ key +'</span><input [disabled]="true" type="range" name="points" min="0" max="3" data-action-type="main-sliders" class="range_status '+ key +' " value="'+ this.credibility_digit_map[key] +'">';
   // }
+
+  trackByIndex(index: number, obj: any): any {
+    return index;
+  }
 }
