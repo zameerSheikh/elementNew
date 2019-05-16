33750fab7cfe2ac8b9ab5aa1c7400dc9f09b670f
Gollapally Karunakar <gollapally@blackswan-technologies.com>
2019-05-16 10:48:18 +0530

    css changes

--- src/app/modules/source-management/source-management.component.ts
+++ src/app/modules/source-management/source-management.component.ts
@@ -9,81 +9,75 @@ import { CommonServicesService } from '../../common-modules/services/common-serv
 
 import { DynamicHeadersRendererComponent } from './template-renderer/dynamic-headers/dynamic-headers-renderer.component';
 import { MediaRendererComponent } from './template-renderer/media-renderer/media-renderer.component';
-//import "ag-grid-enterprise/main";
 
-//import "ag-grid-enterprise/main";
-//import { DynamicHeadersRendererComponent } from './template-render/template-renderer.component';
+//Jquery initialization
 declare var $: any
 
+//Normal variables
 var generalRespData:any = [];
 var newsRespData:any = [];
 var indexRespData:any = [];
+
 @Component({
   selector: 'app-source-management',
   templateUrl: './source-management.component.html',
   styleUrls: ['./source-management.component.scss']
 })
+
 export class SourceManagementComponent implements OnInit {
   @ViewChild("agGrid") agGrid: AgGridNg2;
   @ViewChild("content") modalContent;
   @ViewChild("editContent") editModalContent;
- // @ViewChild('greetCell') greetCell: TemplateRef<any>;
+
+  /**====================== Public variables Start=======================*/
+
   public gridOptions: GridOptions;
-  private agGridLoader: boolean = false;
-  private responseData:any = [];
+
+  /** ===================== public type array ========================== */
+  public columnDefs:any = [];
+  public rowData:any = [];
+  public mainClassificationData:any = [];
   public currentTabData:any = [];
-  public showPopover:Boolean = false;
-  public x_position:number;
-  public y_position:number;
   public dynamicHeadersForDataAttributes:any = [];
-  private totallSourceCount:number = 0;
-  public clsfctn: any;
-  public editData: any;
-  public editDataInitial: any;
+
+  /** ========================== public type booleans ========================== */
+  public showPopover:boolean = false;
+  public isEdit:boolean = false;
+  
+  /** ========================== public type any ========================== */
+  public clsfctn:any;
+  public editData:any;
+  public editDataInitial:any;
+  public editClsfctn: any;
+  public _mainPrams:any;
+  public getRowHeight:any;
+  public defaultColDef:any;
+
+  /** ========================== public type string ========================== */
   public sourcename: string = '';
   public sourcelink: string = '';
-  public editClsfctn: any;
   public editSourcename: string = '';
   public editSourcelink: string = '';
-  public isEdit:boolean = false;
-  private staticHeaders1 = [
-    "Source",
-    "Link",
-    "Domain",
-    "Industry",
-    "Jurisdiction"
-  ];
-  private staticHeaders2:any = ["Media", "Visible", "Edit"];
+
+  /**====================== Public variables end=======================*/
+
+  /**====================== Private variables start=======================*/
+
+  /** ========================== Private type boolean ========================== */
+  private agGridLoader: boolean = false;
+  private fisrtCall:boolean = false;
+
+  /** ========================== Private type Array ========================== */
+  private responseData:any = [];
   private dynamicHeaders:any = [];
-  public _mainPrams:any;
-  public getRowHeight;
-  private gridApi:any;
-  private frameworkComponents;
-  private gridColumnApi:any;
-  public columnDefs:any = [];
-  public defaultColDef:any;
-  private sortingOrder:any;
-  private paginationPageSize:any; 
-  private dynamicDefs:any;
-  public rowData:any = [];
   private finalHeaders:any = [];
-  public mainClassificationData:any = [];
-  private visible:any;
-  private fisrtCall:Boolean = false;
-  private credibility_digit_map:any = {
-    NONE: "0",
-    LOW: "1",
-    MEDIUM: "2",
-    HIGH: "3"
-  };
-  private disabledCell:boolean = false;
-  private credibility_text_map:any = {
-    0: "NONE",
-    1: "LOW",
-    2: "MEDIUM",
-    3: "HIGH"
-  };
-  mouseHandler: any;
+  private staticHeaders1:any = ["Source","Link","Domain","Industry","Jurisdiction"];
+  private staticHeaders2:any = ["Media", "Visible", "Edit"];
+
+  /** ========================== Private type object ========================== */
+  private frameworkComponents:any = {};
+  private credibility_digit_map:any = { NONE: "0",LOW: "1",MEDIUM: "2",HIGH: "3"};
+  private credibility_text_map:any = { 0: "NONE",1: "LOW",2: "MEDIUM",3: "HIGH"};
   private params = {
     "recordsPerPage":10,
     "pageNumber":1,
@@ -94,8 +88,21 @@ export class SourceManagementComponent implements OnInit {
     "visible":'',
   }
 
+  /** ========================== Private type number ========================== */
+  private totallSourceCount:number = 0;
   private recordsPerPage:number = 10;
   private pageNum:number = 1;
+
+  /** ========================== Private type any ========================== */
+  private gridApi:any;
+  private gridColumnApi:any;
+  private paginationPageSize:any; 
+  private visible:any;
+
+  /**====================== Private variables end=======================*/
+
+  /**===================================== Constructor start=====================================*/
+
   constructor(private _sourceManagementService:SourceManagementService,
               private config: NgbTabsetConfig,
               private cmnSrvc: CommonServicesService,
@@ -128,6 +135,10 @@ export class SourceManagementComponent implements OnInit {
       mediaRendererComponent : MediaRendererComponent
     }
   }
+
+  /**===================================== Constructor end=====================================*/
+
+  /**===================================== ngOnInit start=====================================*/
   ngOnInit() {
 
     this.cmnSrvc.addSource.subscribe(
@@ -186,6 +197,9 @@ export class SourceManagementComponent implements OnInit {
     // this.gridOptions.rowData=this.rowData;
   }
 
+  /**===================================== ngOnInit end=====================================*/
+
+  /**===================================== getClassifications function start=====================================*/
   getClassifications(){
     this._sourceManagementService.getClassificationsForScource().subscribe(data => {
       this.mainClassificationData = data;
@@ -196,7 +210,10 @@ export class SourceManagementComponent implements OnInit {
       console.log(error);
     }))
   }
+  /**===================================== getClassifications function end=====================================*/
 
+
+  /**===================================== getSources function start=====================================*/
   getSources(values){
     var params = {
       "recordsPerPage":this.recordsPerPage,
@@ -237,6 +254,10 @@ export class SourceManagementComponent implements OnInit {
       this.totallSourceCount = indexRespData.paginationInformation.totalResults;
     }
   }
+  /**===================================== getSources function end=====================================*/
+
+  /**===================================== tabChange function start=====================================*/
+
   tabChange(index,values){
     this.fisrtCall = true;
     this.responseData = [];
@@ -245,7 +266,10 @@ export class SourceManagementComponent implements OnInit {
     console.log('this.columnDefs: ', this.columnDefs);
     console.log('this.rowData: ', this.rowData);
   }
+  /**===================================== tabChange function end=====================================*/
 
+
+  /**===================================== getAllSources function start=====================================*/
   getAllSources(params1){
     this.agGridLoader = true;
     this._sourceManagementService.getAllSourcesData(this.params).subscribe(data => {
@@ -279,7 +303,9 @@ export class SourceManagementComponent implements OnInit {
       console.log('error', error);
     });
   }
+  /**===================================== getAllSources function end=====================================*/
   
+  /**===================================== onGridReady function start=====================================*/
   /**Getting data from get source api */
   onGridReady(params1){
     this.getClassifications();
@@ -289,7 +315,9 @@ export class SourceManagementComponent implements OnInit {
       this.getAllSources(params1);
     }
   }
+  /**===================================== onGridReady function end=====================================*/
 
+  /**===================================== setTableData function start=====================================*/
   setTableData(responseData){
     console.log('responseData==============>: ', responseData);
     this.columnDefs = [];
@@ -429,13 +457,9 @@ export class SourceManagementComponent implements OnInit {
     }
     return this.rowData;
   }
-  getSlider(key){
-    return '<span class="main-slider-value">'+ key +'</span><input [disabled]="true" type="range" name="points" min="0" max="3" data-action-type="main-sliders" class="range_status '+ key +' " value="'+ this.credibility_digit_map[key] +'">';
-  }
+  /**===================================== setTableData function end =====================================*/
 
-  getEditIcons(key){
-    return `<i class="fa fa-edit f-16 text-dark-cream font-16" data-action-type="edit"></i>`
-  }
+  /**===================================== onRowClicked function start =====================================*/
   
   /**Row actions on click */
   public onRowClicked(e,currentTabData,dynamicHeaders) {
@@ -451,9 +475,9 @@ export class SourceManagementComponent implements OnInit {
           if(actionType == "view"){
             switch(show_class) {
               case "fa fa-eye":
-                  return this.onActionViewClick(data, e);
+                  return this.onActionViewClick(data,true,e,currentTabData.result[e.rowIndex]);
               case "fa fa-eye-slash":
-                  return this.onActionRemoveClick(data, e);
+                  return this.onActionViewClick(data,false,e,currentTabData.result[e.rowIndex]);
             }
           }
           if(actionType == "main-sliders"){
@@ -464,10 +488,12 @@ export class SourceManagementComponent implements OnInit {
           }
       }
   }
-  
-  
-  /**Click Function for each cell in the table*/
 
+  /**===================================== onRowClicked function end =====================================*/
+  
+  
+  /**===================================== onCellClicked function start =====================================*/
+  /**Click Function for each cell in the table*/
   public onCellClicked(e,currentTabDataForApi){
     console.log('e: ', e);
     var className = e.event.target.className.split("f-16")[0].trim();
@@ -503,8 +529,11 @@ export class SourceManagementComponent implements OnInit {
     }
   }
 
-  /**Updating the slider value with API call */
+  /**===================================== onCellClicked function end =====================================*/
 
+
+  /**====================== updateSourceByChangingSlider function start =============================*/
+  /**Updating the slider value with API call */
   public updateSourceByChangingSlider(e,c_data){
     console.log('e,c_data,dynamic_data: ', e,c_data);
     var currentClassificationData = c_data;
@@ -525,38 +554,74 @@ export class SourceManagementComponent implements OnInit {
       console.log(error);
     }));
   }
+  /**====================== updateSourceByChangingSlider function end =============================*/
 
-  public onActionViewClick(data: any, e: any){
-    
-    var ele = e.event.target.parentElement.parentElement.children;
-    for(var i=0; i<ele.length; i++ ){
-      var mainDicClass = ele[i].className;
-      var crrClass = ele[i].children[0].className.split("f-16")[0].split(' ')[1];
-      if(crrClass != "fa-eye")
-      ele[i].className = mainDicClass + " disabledRow";
+  /**===================================== onActionViewClick function start =====================================*/
+
+  public visibleSourceUpdatedData:any=[];
+  public onActionViewClick(data: any,visible, e: any,currentRowData){
+    this.visibleSourceUpdatedData = [];
+    var updatedClassifications:any=[];
+    currentRowData.classifications[0].subClassifications =  currentRowData.classifications[0].subClassifications.map(subClas=>{
+      subClas.dataAttributes = [];
+      return subClas;
+    });
+
+    if(visible){
+      var ele = e.event.target.parentElement.parentElement.children;
+      for(var i=0; i<ele.length; i++ ){
+        var mainDicClass = ele[i].className;
+        var crrClass = ele[i].children[0].className.split("f-16")[0].split(' ')[1];
+        if(crrClass != "fa-eye")
+        ele[i].className = mainDicClass + " disabledRow";
+      }
+  
+      e.event.target.className = "fa fa-eye-slash f-16 text-dark-cream font-16";
+      // this.dataService.changeMessage(e.data.visible);
+      currentRowData.classifications[0].hideStatusDto.visible = visible;
+      updatedClassifications.push(currentRowData.classifications[0]);
+      this.visibleSourceUpdatedData = {
+        "classifications" : updatedClassifications,
+        "deleteMedia" : currentRowData.deleteMedia,
+        "entityId" : currentRowData.entityId,
+        "sourceCreatedDate" : currentRowData.sourceCreatedDate,
+        "sourceDisplayName" : currentRowData.sourceDisplayName,
+        "sourceDomain" : [],
+        "sourceId" : currentRowData.sourceId,
+        "sourceIndustry" : [],
+        "sourceJurisdiction" : [],
+        "sourceMedia" : [],
+        "sourceName" : currentRowData.sourceName,
+        "sourceType" : currentRowData.sourceType,
+        "sourceUrl" : currentRowData.sourceUrl
+      };
+    }
+    else{
+      var ele = e.event.target.parentElement.parentElement.children;
+      for(var i=0; i<ele.length; i++ ){
+        var mainDivClass = ele[i].className.replace(/ disabledRow/g, "");
+        // var mainDivClass = ele[i].className.replace(" disabledRow", "");
+        // var crrClass = ele[i].children[0].className.split("f-16")[0].split(' ')[1];
+        var crrClass = e.event.target.className.split("f-16")[0].split(' ')[1];
+        if(crrClass != "fa-eye")
+            ele[i].className = mainDivClass;
+      }
+      e.event.target.className = "fa fa-eye f-16 text-dark-cream font-16";
+      currentRowData.classifications[0].hideStatusDto.visible = visible;
+      this.visibleSourceUpdatedData = currentRowData
     }
 
-    e.event.target.className = "fa fa-eye-slash f-16 text-dark-cream font-16";
-    // this.dataService.changeMessage(e.data.visible);
-    console.log("onActionViewClick...", e, this.visible);
+    var params = this.visibleSourceUpdatedData;
+      this._sourceManagementService.updateScource(params).subscribe(data => {
+        console.log("Success........!");
+      },
+      (error => {
+        console.log(error);
+      }));
+    console.log("this.visibleSourceUpdatedData",this.visibleSourceUpdatedData);
 }
 
-public onActionRemoveClick(data: any, e: any){
-
-  var ele = e.event.target.parentElement.parentElement.children;
-  for(var i=0; i<ele.length; i++ ){
-    var mainDivClass = ele[i].className.replace(/ disabledRow/g, "");
-    // var mainDivClass = ele[i].className.replace(" disabledRow", "");
-    // var crrClass = ele[i].children[0].className.split("f-16")[0].split(' ')[1];
-    var crrClass = e.event.target.className.split("f-16")[0].split(' ')[1];
-    if(crrClass != "fa-eye")
-        ele[i].className = mainDivClass;
-  }
-
-  e.event.target.className = "fa fa-eye f-16 text-dark-cream font-16";
-  // this.dataService.changeMessage(e.data.visible);
-  console.log("onActionRemoveClick...", e, this.visible);
-}
+/**===================================== onActionViewClick function end =====================================*/
 
 public onSliderChange(data: any, e: any,currentTabDataForApi,dynamicHeaders){
     const currentVal = e.event.target.value;
@@ -565,10 +630,13 @@ public onSliderChange(data: any, e: any,currentTabDataForApi,dynamicHeaders){
 public getDisplayedColumnsAfter(e: any){
   console.log("getDisplayedColumnsAfter...", e);
 }
- getAllColumns(e: any){
-  console.log("getAllColumns()...", e);
-}
 
+getSlider(key){
+  return '<span class="main-slider-value">'+ key +'</span><input [disabled]="true" type="range" name="points" min="0" max="3" data-action-type="main-sliders" class="range_status '+ key +' " value="'+ this.credibility_digit_map[key] +'">';
+}
+getEditIcons(key){
+  return `<i class="fa fa-edit f-16 text-dark-cream font-16" data-action-type="edit"></i>`
+}
 onBtExport() {
   var params = {
     skipHeader: false, //getBooleanValue("#skipHeader"),
@@ -604,35 +672,29 @@ onBtExport() {
   this.gridApi.exportDataAsCsv(params);
 }
 
-// Add source starts
+/**===================================== Add source function start =====================================*/
 
   domainList = [];
-  // editSelectedDomains = [];
   selectedDomains = [];
   jurisdictionList = [];
-  // editSelectedJurisdictions = [];
   selectedJurisdictions = [];
   mediaList = [];
-  // editSelectedMedias = [];
   selectedMedias = [];
   industryList = [];
-  // editSelectedIndustries = [];
   selectedIndustries = [];
   domainSettings = {};
   jurisdictionSettings = {};
   mediaSettings = {};
   industrySettings = {};
 
-
-      initializeScroll(){
-        $(".ng-multiselect-wrapper").click(function(){
-          $(".c-list").mThumbnailScroller({
-            axis:"x"
-          });
-        });
-      }
-      
-
+  initializeScroll(){
+    $(".ng-multiselect-wrapper").click(function(){
+      $(".c-list").mThumbnailScroller({
+        axis:"x"
+      });
+    });
+  }
+  
   onItemSelect(item:any){
     console.log(item);
     this.initializeScroll();
@@ -712,7 +774,7 @@ onBtExport() {
     }
   }
 
-// Add source ends
+/**===================================== Add source function end =====================================*/
 
 }
 
